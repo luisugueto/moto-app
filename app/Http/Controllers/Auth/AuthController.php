a4d0f75c5b2687183b02e416945c8965cdc690c8
Luis Ugueto <blink242@outlook.com>
2021-03-05 17:21:23 -0400

    Update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            // 'user_type_id' => 2,
+            'user_type_id' => 2,
         ]);
     }
 }
