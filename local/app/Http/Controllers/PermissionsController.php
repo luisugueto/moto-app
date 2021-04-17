cbb7e86faa5f1b4fb7ffa74a95e88dd23a004422
Luis Ugueto <blink242@outlook.com>
2021-04-17 16:38:14 -0400

    Update permissions

--- local/app/Http/Controllers/PermissionsController.php
+++ local/app/Http/Controllers/PermissionsController.php
@@ -7,7 +7,9 @@ use App\Http\Requests;
 use App\Permission;
 use App\Menu;
 use App\Role;
+use App\PermissionsMenu;
 use DB;
+use Redirect;
 
 class PermissionsController extends Controller
 {
@@ -22,14 +24,16 @@ class PermissionsController extends Controller
         $permission = Permission::get()->take(4);    
         $menus = DB::table('menus')->get();
         $roles = Role::get();
+        $permissionsMenu = PermissionsMenu::all();
+        
         // $roles = DB::table('roles')
         // ->join('permission_role', 'permission_role.role_id', '=', 'roles.id')
         // ->join('permissions', 'permission_role.permission_id', '=', 'permissions.id')
         // ->select('roles.*', DB::raw('group_concat(permissions.display_name) as permisos'))
         // ->groupBy('roles.id')
         // ->get();
-      
-        return view('backend.permissions.index', compact('permission', 'menus','roles'));
+    
+        return view('backend.permissions.index', compact('permission', 'permissionsMenu','menus','roles'));
     }
 
     /**
@@ -50,7 +54,29 @@ class PermissionsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $roles = Role::count();
+        $menus = DB::table('menus')->count();
+       
+        for($i = 1; $i <= $roles; $i++){ 
+            for($j = 1; $j <= $menus; $j++){
+                $menu = PermissionsMenu::where('roles_id', $i)->where('menus_id', $j)->first();
+        
+                $menuPermission = '';
+                foreach($request['permissionRol'.$i.'_'.$j] as $key => $value){
+                    if($key == 0)
+                        $menuPermission.=$value;
+                    else
+                        $menuPermission.=','.$value;
+                }
+                $menu = PermissionsMenu::find($menu->id);
+
+                $menu->permissions = $menuPermission;
+                $menu->update();
+            }
+        }
+            
+        return Redirect::to('/permisos')->with('notification', 'Permisos actualizados exitosamente!');
+
     }
 
     /**
