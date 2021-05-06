67da104903f29878fcf35b7f65a9912fd462ea2b
Aldrha Petit <aldrha@gmail.com>
2021-05-06 12:07:00 -0400

    fix carrousel

--- local/resources/views/backend/purchase_valuation/ficha.blade.php
+++ local/resources/views/backend/purchase_valuation/ficha.blade.php
@@ -1045,14 +1045,14 @@ aria-hidden="true">
 
             <div class="main-card mb-3 card">
                 <div class="card-body">
-                    <div id="carouselExampleControls1" class="carousel slide" data-ride="carousel">
+                    <div id="lightbox" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner" id="imagesCarrousel">
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls1" role="button" data-slide="prev">
+                        <a class="carousel-control-prev" href="#lightbox"  role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls1" role="button" data-slide="next">
+                        <a class="carousel-control-next" href="#lightbox"  role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>
