1cfe1d120cb4e60eccd568562eb623a38ab00a24
Luis Ugueto <blink242@outlook.com>
2021-05-31 13:53:31 -0400

    Update send ficha email

--- local/resources/views/backend/emails/send-document-firma.blade.php
+++ local/resources/views/backend/emails/send-document-firma.blade.php
@@ -171,7 +171,7 @@
                                             </div>
                                             <div style="color:#555555;font-family:Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.5;padding-top:10px;padding-right:40px;padding-bottom:10px;padding-left:40px;">
                                                 <div style="line-height: 1.5; font-size: 12px; font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; color: #555555; mso-line-height-alt: 18px;">
-                                                    <p style="text-align: justify; line-height: 1.5; word-break: break-word; font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; font-size: 16px; mso-line-height-alt: 24px; margin: 0;"><span style="font-size: 16px; color: #6d89bc;">NUEVA SOLICITUD.</span></p>
+                                                    <p style="text-align: justify; line-height: 1.5; word-break: break-word; font-family: Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; font-size: 16px; mso-line-height-alt: 24px; margin: 0;"><span style="font-size: 16px; color: #6d89bc;"></span></p>
 
                                                    
                                                 </div>
