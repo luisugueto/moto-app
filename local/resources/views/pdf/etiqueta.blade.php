44997ba93c0b79511c05ba41f5a9b81c5e555e19
Aldrha Petit <aldrha@gmail.com>
2021-07-16 14:07:41 -0400

    change F to L in etiquetas

--- local/resources/views/pdf/etiqueta.blade.php
+++ local/resources/views/pdf/etiqueta.blade.php
@@ -26,12 +26,12 @@
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 6pt;width:700px;" cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s1" style="padding-left: 19pt;padding-right: 76pt;text-indent: 0pt;text-align: center;"><span>NºExpediente: F{{ $purchase->id }}<br/>Marca: {{ strtoupper($purchase->brand) }}</span></p>
+                        <p class="s1" style="padding-left: 19pt;padding-right: 76pt;text-indent: 0pt;text-align: center;"><span>NºExpediente: L{{ $purchase->id }}<br/>Marca: {{ strtoupper($purchase->brand) }}</span></p>
                         <p class="s1" style="padding-left: 19pt;padding-right: 76pt;text-indent: 0pt;text-align: center;"><span>Modelo:  {{ $purchase_management->model }}<br/>Matricula: {{ $purchase_management->registration_number }}</span></p>
                         <p class="s1" style="padding-left: 9pt;padding-right: 66pt;text-indent: 0pt;line-height: 15pt;text-align: center;">Bastidor: {{ $purchase_management->frame_no }}</p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s1" style="padding-left: 19pt;padding-right: 76pt;text-indent: 0pt;text-align: center;"><span>NºExpediente: F{{ $purchase->id }}<br/>Marca: {{ strtoupper($purchase->brand) }}</span></p>
+                        <p class="s1" style="padding-left: 19pt;padding-right: 76pt;text-indent: 0pt;text-align: center;"><span>NºExpediente: L{{ $purchase->id }}<br/>Marca: {{ strtoupper($purchase->brand) }}</span></p>
                         <p class="s1" style="padding-left: 19pt;padding-right: 76pt;text-indent: 0pt;text-align: center;"><span>Modelo:  {{ $purchase_management->model }}<br/>Matricula: {{ $purchase_management->registration_number }}</span></p>
                         <p class="s1" style="padding-left: 9pt;padding-right: 66pt;text-indent: 0pt;line-height: 15pt;text-align: center;">Bastidor: {{ $purchase_management->frame_no }}</p>
                     </td>
@@ -40,70 +40,70 @@
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px; " cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px " cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px " cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px " cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px " cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                    <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                    <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px " cellspacing="0">
             <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
             <table style="border-collapse:collapse;margin-left:40.624pt;margin-top: 4pt;width:700px " cellspacing="0">
                 <tr style="height:35mm;">
                     <td style="width:251pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                     <td style="width:252pt">
-                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>F{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
+                        <p class="s2" style="padding-top: 3pt;padding-left: 1pt;padding-right: 38pt;text-indent: 0pt;text-align: center;"><span>L{{ $purchase->id }} {{ strtoupper($purchase->brand) }}<br/>{{ strtoupper(nl2br($purchase_management->model)) }}</span></p>
                     </td>
                 </tr>
             </table>
